5ffdc05ba97ce32d26e485c1d1891d365ba8e0fd
Francisco Sancho <frsancho1985@gmail.com>
2023-10-14 12:46:36 -0300

    Adding full project

---
+++ src/main/java/br/com/sancho/todolist/errors/ExcepetionHandlerController.java
@@ -0,0 +1,15 @@
+package br.com.sancho.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExcepetionHandlerController {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMostSpecificCause().getMessage());
+    }
+}
